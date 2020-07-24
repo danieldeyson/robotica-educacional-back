0741fdeea8eee153bb6eceba55ebf6125f057c70
Hugo Dionizio Santos <dnhutos@hotmail.com>
2020-07-24 05:56:47 +0100

    Corrigindo classes

--- src/main/java/com/example/robotica/repository/UsersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.robotica.repository;
-
-import com.example.robotica.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-
-    Users findByLogin(String login);
-}
