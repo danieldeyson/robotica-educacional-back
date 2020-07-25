f381e2f35a8c6e4c8949df86962d929a1c37ae7d
Hugo Dionizio Santos <dnhutos@hotmail.com>
2020-07-25 02:03:53 +0100

    Hello Spring-Boot

---
+++ src/main/java/com/example/robotica/DemoApplication.java
@@ -0,0 +1,24 @@
+package com.example.robotica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+@RestController
+class HelloController {
+
+	@GetMapping("/")
+	String hello() {
+		return "Hello World";
+	}
+}
--- src/main/java/com/example/robotica/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.robotica.repository;
-
-import com.example.robotica.Model.Professor;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Integer> {
-    
-}
